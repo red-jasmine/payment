9a9f0ff50e3d51593462e30615dce25ef7c006e4
liushoukun <liushoukun66@gmail.com>
2025-01-01 21:54:17 +0800

    refactor(payment): 重构支付结果数据结构
    - 将 code、message、data 等通用属性和方法移至 AbstractChannelResult 类
    - 清理 ChannelPurchaseResult 和 ChannelRefundResult 类中的冗余代码
    - 优化类结构，提高代码复用性和可维护性

--- src/Domain/Gateway/Data/ChannelPurchaseResult.php
+++ src/Domain/Gateway/Data/ChannelPurchaseResult.php
@@ -9,53 +9,8 @@ class ChannelPurchaseResult extends AbstractChannelResult
 
     public ?PaymentTrigger $paymentTrigger = null;
 
-    public mixed   $result  = null;
-    public ?string $code    = null;
-    public ?string $message = null;
-
 
     public ?string $tradeNo = null;
-
-    public function getCode() : ?string
-    {
-        return $this->code;
-    }
-
-    public function setCode(?string $code) : static
-    {
-        $this->code = $code;
-        return $this;
-    }
-
-    public function getMessage() : ?string
-    {
-        return $this->message;
-    }
-
-    public function setMessage(?string $message) : static
-    {
-        $this->message = $message;
-        return $this;
-    }
-
-
-    /**
-     * The data contained in the response.
-     *
-     * @var mixed
-     */
-    protected mixed $data;
-
-    public function getData() : mixed
-    {
-        return $this->data;
-    }
-
-    public function setData(mixed $data) : static
-    {
-        $this->data = $data;
-        return $this;
-    }
 
 
     public function getTradeNo() : ?string
@@ -66,18 +21,6 @@ class ChannelPurchaseResult extends AbstractChannelResult
     public function setTradeNo(?string $tradeNo) : static
     {
         $this->tradeNo = $tradeNo;
-        return $this;
-    }
-
-
-    public function getResult() : mixed
-    {
-        return $this->result;
-    }
-
-    public function setResult(mixed $result) : static
-    {
-        $this->result = $result;
         return $this;
     }
 
--- src/Domain/Gateway/Data/ChannelRefundResult.php
+++ src/Domain/Gateway/Data/ChannelRefundResult.php
@@ -7,52 +7,11 @@ class ChannelRefundResult extends AbstractChannelResult
 
 
     public mixed   $result  = null;
-    public ?string $code    = null;
-    public ?string $message = null;
+
 
 
     public ?string $tradeNo = null;
 
-    public function getCode() : ?string
-    {
-        return $this->code;
-    }
-
-    public function setCode(?string $code) : static
-    {
-        $this->code = $code;
-        return $this;
-    }
-
-    public function getMessage() : ?string
-    {
-        return $this->message;
-    }
-
-    public function setMessage(?string $message) : static
-    {
-        $this->message = $message;
-        return $this;
-    }
-
-
-    /**
-     * The data contained in the response.
-     *
-     * @var mixed
-     */
-    protected mixed $data;
-
-    public function getData() : mixed
-    {
-        return $this->data;
-    }
-
-    public function setData(mixed $data) : static
-    {
-        $this->data = $data;
-        return $this;
-    }
 
 
     public function getTradeNo() : ?string
