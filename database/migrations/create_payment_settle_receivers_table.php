9997944e7eb6e395f1e1af7ec30143aba85a9035
liushoukun <liushoukun66@gmail.com>
2025-01-16 12:00:42 +0800

    feat(payment): 重构结算分账接收方数据结构
    - 更新 payment_settle_receivers 表结构，增加接收方用户类型、接收方用户ID等字段
    - 添加唯一索引，确保商户应用与渠道的唯一性
    - 删除 SettleAccountData 类，简化数据结构

--- database/migrations/create_payment_settle_receivers_table.php
+++ database/migrations/create_payment_settle_receivers_table.php
@@ -3,32 +3,44 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use RedJasmine\Payment\Domain\Models\Enums\AccountTypeEnum;
 use RedJasmine\Payment\Domain\Models\Enums\CertTypeEnum;
 use RedJasmine\Payment\Domain\Models\Enums\SettleRelationTypeEnum;
 
 return new class extends Migration {
     public function up() : void
     {
-        Schema::create(config('red-jasmine-payment.tables.prefix', 'jasmine_') . 'payment_settle_receivers',
+        Schema::create(config('red-jasmine-payment.tables.prefix', 'jasmine_').'payment_settle_receivers',
             function (Blueprint $table) {
                 $table->unsignedBigInteger('id')->primary()->comment('ID');
                 $table->unsignedBigInteger('system_merchant_app_id')->comment('系统商户ID');
-                $table->string('relation_type')->comment(SettleRelationTypeEnum::comments('关系类型'));
-                $table->string('name')->comment('名称');
+                $table->string('receiver_type', 32)->comment('接收方用户类型');
+                $table->string('receiver_id', 64)->comment('接收方用户ID');
+                $table->string('channel_code', 64)->comment('渠道编码');
+                $table->string('channel_merchant_id', 64)->comment('渠道商户号');
+                $table->text('name')->comment('名称');
+                $table->string('relation_type')->default(SettleRelationTypeEnum::CUSTOM)->comment(SettleRelationTypeEnum::comments('关系类型'));
+                $table->string('account_type')->comment(AccountTypeEnum::comments('账户类型'));
+                $table->string('account')->comment('账号');
                 $table->string('cert_type')->nullable()->comment(CertTypeEnum::comments('证件类型'));
-                $table->string('cert_no')->nullable()->comment('收款方证件号');
-
+                $table->text('cert_no')->nullable()->comment('收款方证件号');
                 $table->string('creator_type', 32)->nullable();
                 $table->string('creator_id', 64)->nullable();
                 $table->string('updater_type', 32)->nullable();
                 $table->string('updater_id', 64)->nullable();
                 $table->timestamps();
+                $table->unique([
+                    'system_merchant_app_id',
+                    'receiver_type', 'receiver_id', 'channel_code', 'channel_merchant_id'
+                ], 'uk_merchant_app_channel');
+
+                $table->comment('支付-结算分账接收方');
             });
     }
 
     public function down() : void
     {
         Schema::dropIfExists(config('red-jasmine-payment.tables.prefix',
-                                    'jasmine_') . 'payment_settle_receivers');
+                'jasmine_').'payment_settle_receivers');
     }
 };
